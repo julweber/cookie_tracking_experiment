39f2c98ee49531633048488d26ca40ccc93c1156
Julian Weber <julianweberdev@gmail.com>
2017-01-22 20:48:26 +0100

    cleanup, subviews in tracked app

--- tracked/app.rb
+++ tracked/app.rb
@@ -8,3 +8,11 @@ set :port, 1235
 get '/' do
   File.read(File.join('views', 'index.html'))
 end
+
+get '/superproduct' do
+  File.read(File.join('views', 'superproduct.html'))
+end
+
+get '/superservice' do
+  File.read(File.join('views', 'superservice.html'))
+end
--- tracker/app.rb
+++ tracker/app.rb
@@ -6,16 +6,29 @@ require 'pp'
 disable :protection
 set :port, 1234
 
+def set_tracking_cookie
+  if cookies[:tracking_id].nil?
+    pp "Setting cookie tracking id!"
+    cookies[:tracking_id] = 'myId'
+  else
+    pp "Found tracking cookie: #{cookies[:tracking_id]} !!!"
+  end
+end
+
+def print_request_infos
+  pp "Request Referrer: #{request.referrer}"
+  pp "Cookies: #{cookies.to_a.join(" : ")}"
+  pp "Cookie tracking id: #{cookies[:tracking_id]}"
+end
 
 get '/' do
-  pp cookies
+  print_request_infos
+  set_tracking_cookie
   File.read(File.join('views', 'index.html'))
 end
 
 get '/iframe' do
-  pp cookies[:tracking_id]
-  cookies[:tracking_id] = 'myId' if cookies[:tracking_id].nil?
-
-  pp cookies
+  print_request_infos
+  set_tracking_cookie
   File.read(File.join('views', 'iframe.html'))
 end
